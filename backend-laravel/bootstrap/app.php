7510336d2beae8ac83185a8f5b91c0d2c802fcfe
Paola <tj.paola.tapia.g@upds.net.bo>
2026-09-07 17:57:55 -0400

    Register CORS middleware on API group

--- backend-laravel/bootstrap/app.php
+++ backend-laravel/bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(\App\Http\Middleware\EnsureCors::class);
+        $middleware->api(prepend: [
+            \App\Http\Middleware\EnsureCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
